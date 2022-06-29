19c16427803fb4d1f93fa7c677813c9ce739aee5
Rimon <rimons2804@gmail.com>
2022-06-29 12:48:33 +0300

    up to date

--- js/views/home-page.page.cmp.js
+++ js/views/home-page.page.cmp.js
@@ -2,14 +2,8 @@ import appHeader from '../cmps/main-app/app-header.cmp.js'
 import appFooter from '../cmps/main-app/app-footer.cmp.js'
 
 export default {
-<<<<<<< HEAD
 	props: [],
 	template: `
-=======
-    props: [],
-    template: `
-    <app-header></app-header>
->>>>>>> 4bdf71036c2794e1ce9d07083d63fb4e8d9ce0b6
     <section class="main-layout">
         
         <h1>Home-Page</h1>
@@ -20,7 +14,6 @@ export default {
     </section>
     <app-footer></app-footer>
 `,
-<<<<<<< HEAD
 	data() {
 		return {}
 	},
@@ -31,18 +24,3 @@ export default {
 	unmounted() {},
 	components: {},
 }
-=======
-    data() {
-        return {};
-    },
-    methods: {},
-    computed: {},
-    created() { },
-    mounted() { },
-    unmounted() { },
-    components: {
-        appHeader,
-        appFooter
-    },
-}
->>>>>>> 4bdf71036c2794e1ce9d07083d63fb4e8d9ce0b6
