be73608faa9c9a2df2ebe0bcb0c1268a7d2530ef
Rimon <rimons2804@gmail.com>
2022-06-29 12:09:47 +0300

    email wired

---
+++ js/cmps/email/email-nav.js
@@ -0,0 +1,17 @@
+export default {
+    props: [],
+    template: `
+    <section class="email-nav">
+        <h1>email-nav</h1>
+    </section>
+`,
+    data() {
+        return {};
+    },
+    methods: {},
+    computed: {},
+    created() { },
+    mounted() { },
+    unmounted() { },
+    components: {},
+}
